005eaa3a8237d5b887d8941c96a8cd21b51a19ed
Jamesford <jamiestobbs92@gmail.com>
2015-12-18 20:16:21 +0800

    Basic ajax page change detection

--- contentscript.js
+++ contentscript.js
@@ -1,50 +1,76 @@
 // Avoid recursive frame insertion...
-var extensionOrigin = 'chrome-extension://' + chrome.runtime.id;
+var extensionOrigin = 'chrome-extension://' + chrome.runtime.id
 if (!location.ancestorOrigins.contains(extensionOrigin)) {
-  // Supported File Types
-  var fileTypes = ['.pdf', '.doc', '.xls', '.ppt', 'docs.google.com'];
 
-  var links = document.getElementsByTagName('a');
-  var fileLinks = [];
-  var gDocsLinks = [];
+  // Inserts gdocs-preview(s) on page
+  function insertPreviews () {
+    // Supported File Types
+    var fileTypes = ['.pdf', '.doc', '.xls', '.ppt', 'docs.google.com']
 
-  Object.keys(links).forEach(function (id) {
-    var link = links[id];
-    fileTypes.some(function (ext) {
-      if (link.href.indexOf(ext) > -1) {
-        if (ext === 'docs.google.com') {
-          gDocsLinks.push(link)
-        } else {
-          fileLinks.push(link);
+    var links = document.getElementsByTagName('a')
+    var fileLinks = []
+    var gDocsLinks = []
+
+    Object.keys(links).forEach(function (id) {
+      var link = links[id]
+      fileTypes.some(function (ext) {
+        if (link.href.indexOf(ext) > -1) {
+          if (ext === 'docs.google.com') {
+            gDocsLinks.push(link)
+          } else {
+            fileLinks.push(link)
+          }
+          return true
         }
-        return true;
-      }
-    });
-  });
+      })
+    })
 
-  fileLinks.forEach(function (link) {
-    var iframe = document.createElement('iframe');
+    fileLinks.forEach(function (link) {
+      var iframe = document.createElement('iframe')
 
-    // Must be declared at web_accessible_resources in manifest.json
-    iframe.src = chrome.runtime.getURL('frame.html') + '?url=' + link.href;
+      // Must be declared at web_accessible_resources in manifest.json
+      iframe.src = chrome.runtime.getURL('frame.html') + '?url=' + link.href
 
-    // Basic Style
-    iframe.style.cssText = 'display:block;height:600px;width:664px;z-index:99999;border:none;margin-top:10px;margin-bottom:10px;';
+      // Basic Style
+      iframe.style.cssText = 'display:block;height:600px;width:664px;z-index:99999;border:none;margin-top:10px;margin-bottom:10px;'
 
-    // Insert after Link to include in Google doc viewer
-    link.parentNode.insertBefore(iframe, link.nextSibling);
-  });
+      // Insert after Link to include in Google doc viewer
+      link.parentNode.insertBefore(iframe, link.nextSibling)
+    })
 
-  gDocsLinks.forEach(function (link) {
-    var iframe = document.createElement('iframe');
+    gDocsLinks.forEach(function (link) {
+      var iframe = document.createElement('iframe')
 
-    // Must be declared at web_accessible_resources in manifest.json
-    iframe.src = chrome.runtime.getURL('frame.html') + '?doc=' + link.href;
+      // Must be declared at web_accessible_resources in manifest.json
+      iframe.src = chrome.runtime.getURL('frame.html') + '?doc=' + link.href
 
-    // Basic Style
-    iframe.style.cssText = 'display:block;height:600px;width:664px;z-index:99999;border:none;margin-top:10px;margin-bottom:10px;';
+      // Basic Style
+      iframe.style.cssText = 'display:block;height:600px;width:664px;z-index:99999;border:none;margin-top:10px;margin-bottom:10px;'
 
-    // Insert after Link to include in Google doc viewer
-    link.parentNode.insertBefore(iframe, link.nextSibling);
-  });
+      // Insert after Link to include in Google doc viewer
+      link.parentNode.insertBefore(iframe, link.nextSibling)
+    })
+  }
+
+  // Insert previews on page load
+  insertPreviews()
+
+  // Watch for ajax page changes and re-insert previews
+  var oldLocation = location.href
+  var interval = setInterval(function() {
+    if (location.href != oldLocation) {
+      console.info(
+        'gdocs-preview detected a page change without a full reload. \n' +
+        'If you see any errors please create a bug report @ https://github.com/wiredcraft/gdocs-preview/issues \n' +
+        'Include the following URL in your report: ' + location.href
+      )
+      oldLocation = location.href
+      insertPreviews()
+    }
+  }, 1000) // check every second
+
+  // If pages changes clear interval
+  window.addEventListener('onbeforeunload', function () {
+    clearInterval(interval)
+  })
 }
