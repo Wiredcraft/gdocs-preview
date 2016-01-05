74eef9fa25bdce1781322b0e247bf52e43d11dc0
Jamesford <jamiestobbs92@gmail.com>
2016-01-05 11:45:04 +0800

    Organise assets files & extension files

--- contentscript.js
+++ contentscript.js
@@ -39,7 +39,7 @@ if (!location.ancestorOrigins.contains(extensionOrigin)) {
       var iframe = document.createElement('iframe')
 
       // Must be declared at web_accessible_resources in manifest.json
-      iframe.src = chrome.runtime.getURL('frame.html') + '?type=' + item.type + '&href=' + item.link.href
+      iframe.src = chrome.runtime.getURL('embed/frame.html') + '?type=' + item.type + '&href=' + item.link.href
 
       // Basic Style
       iframe.style.cssText = iframeStyle(item.link.parentElement.clientWidth)
@@ -47,7 +47,8 @@ if (!location.ancestorOrigins.contains(extensionOrigin)) {
       // Insert after Link to include in Google doc viewer
       item.link.parentNode.insertBefore(iframe, item.link.nextSibling)
 
-      return { iframe: iframe }
+      // Mapped to item in active embeds
+      return { type: item.type, link: item.link, iframe: iframe }
     })
 
     // Set badgeText to embed count
@@ -63,11 +64,6 @@ if (!location.ancestorOrigins.contains(extensionOrigin)) {
   var oldLocation = location.href
   var interval = setInterval(function() {
     if (location.href != oldLocation) {
-      console.info(
-        'gdocs-preview detected a page change without a full reload. \n' +
-        'If you see any errors please create a bug report @ https://github.com/wiredcraft/gdocs-preview/issues \n' +
-        'Include the following URL in your report: ' + location.href
-      )
       oldLocation = location.href
       insertPreviews()
     }
